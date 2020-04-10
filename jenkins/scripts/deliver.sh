84a21adf032c3c08577a30eea273bf7bce13de0b
kishoreprabhu97@gmail.com <kishoreprabhu97@gmail.com>
2020-04-10 20:17:25 +0800

    update

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -19,6 +19,7 @@ echo 'of the previously run process (i.e. "npm start") and writes this value to'
 echo 'the file ".pidfile".'
 set -x
 yarn start &
+curl http://localhost:3000 &
 sleep 1
 echo $! > .pidfile
 set +x

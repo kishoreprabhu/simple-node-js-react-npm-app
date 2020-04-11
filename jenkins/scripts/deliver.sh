57c324a40cbb4d60a80ae4c56666e3924a0dc001
kishoreprabhu97@gmail.com <kishoreprabhu97@gmail.com>
2020-04-11 18:29:27 +0800

    script file changed

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,12 +1,12 @@
+ 
 #!/usr/bin/env sh
 
 echo 'The following "npm" command builds your Node.js/React application for'
-echo 'production in the local "build" directory (i.e. within the'
-echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
-echo 'correctly bundles React in production mode and optimizes the build for'
-echo 'the best performance.'
+echo 'production in the local "build" directory (i.e. within the appropriate'
+echo 'subdirectory of "/var/jenkins_home/workspace/"), correctly bundles React'
+echo 'in production mode and optimizes the build for the best performance.'
 set -x
-yarn run build
+npm run build
 set +x
 
 echo 'The following "npm" command runs your Node.js/React application in'
@@ -18,8 +18,7 @@ echo 'is followed by another command that retrieves the process ID (PID) value'
 echo 'of the previously run process (i.e. "npm start") and writes this value to'
 echo 'the file ".pidfile".'
 set -x
-yarn start &
-curl http://localhost:3000 &
+npm start &
 sleep 1
 echo $! > .pidfile
 set +x
